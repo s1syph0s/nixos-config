f9a976a21e6ab2e50d6748a097b33638c4c34b42
Pasha Fistanto <pasha@fstn.top>
2024-04-17 22:07:30 +0200

    feat: add new keybindings to tmux

--- common/tmux/default.nix
+++ common/tmux/default.nix
@@ -16,6 +16,13 @@
       set -s escape-time 0
       bind-key C-Space send-prefix
       set -g status-style 'bg=#333333 fg=#5eacd3'
+
+      bind -T copy-mode-vi v send-keys -X begin-selection
+      bind -T copy-mode-vi y send-keys -X copy-pipe-and-cancel 'wl-copy'
+
+      bind '"' split-window -v -c "#{pane_current_path}"
+      bind '%' split-window -h -c "#{pane_current_path}"
+      bind 'c' new-window -c "#{pane_current_path}"
     '';
   };
 }
