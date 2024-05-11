92be984938dc3d9fa8d3db3bc0133cb1f227b95e
Pasha Fistanto <pasha@fstn.top>
2024-05-11 15:05:56 +0200

    fix: add zellij keybindings

--- common/zellij/default.nix
+++ common/zellij/default.nix
@@ -4,6 +4,21 @@
     enable = true;
     settings = {
       default_shell = "fish";
+      default_mode = "locked";
+      copy_command = "wl-copy";
+      keybinds.shared_except = {
+        _args = [ "locked" ];
+        bind = {
+          _args = [ "Alt g" ];
+          SwitchToMode = "locked";
+        };
+      };
+      keybinds.locked = {
+        bind = {
+          _args = [ "Alt g" ];
+          SwitchToMode = "normal";
+        };
+      };
     };
   };
 }
