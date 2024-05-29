30ab6424e785f55bc1bb922319f833107d19b54b
Pasha Fistanto <pasha@fstn.top>
2024-05-29 06:27:09 +0200

    feat: enable wireguard

--- host/greenbox/configuration.nix
+++ host/greenbox/configuration.nix
@@ -187,6 +187,20 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  # enable wireguard
+  networking.firewall = {
+    # if packets are still dropped, they will show up in dmesg
+    logReversePathDrops = true;
+    # wireguard trips rpfilter up
+    extraCommands = ''
+      ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport 1637 -j RETURN
+      ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --dport 1637 -j RETURN
+    '';
+    extraStopCommands = ''
+      ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport 1637 -j RETURN || true
+      ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport 1637 -j RETURN || true
+    '';
+  };
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
