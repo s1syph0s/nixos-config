1cac608ca5d7b24628ea68b58ca573650d7d0aa3
Pasha Fistanto <pasha@fstn.top>
2024-08-18 15:25:49 +0200

    [neovim] change mapping

--- common/neovim/lua/cmp.lua
+++ common/neovim/lua/cmp.lua
@@ -45,24 +45,18 @@ cmp.setup {
         fallback()
       end
     end, {'i'}),
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_locally_jumpable() then
+    ['<Tab>'] = cmp.mapping.select_next_item(),
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+    ['<C-l>'] = cmp.mapping(function()
+      if luasnip.expand_or_locally_jumpable() then
         luasnip.expand_or_jump()
-      else
-        fallback()
       end
-    end, {'i', 's'}),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.locally_jumpable(-1) then
+    end, { 'i', 's' }),
+    ['<C-h>'] = cmp.mapping(function()
+      if luasnip.locally_jumpable(-1) then
         luasnip.jump(-1)
-      else
-        fallback()
       end
-    end, {'i', 's'}),
+    end, { 'i', 's' }),
   },
   sources = {
     { name = 'nvim_lsp' },
