4b9223d37e96b1fa2f58263747ea738923301ba5
Pasha Fistanto <pasha@fstn.top>
2025-02-06 18:11:37 +0100

    chore: formatting

--- flake.nix
+++ flake.nix
@@ -16,89 +16,76 @@
     envfs.url = "github:Mic92/envfs";
     envfs.inputs.nixpkgs.follows = "nixpkgs";
 
-    zjstatus = {
-      url = "github:dj95/zjstatus";
-    };
+    zjstatus = { url = "github:dj95/zjstatus"; };
   };
 
-  outputs = { self, nixpkgs, home-manager, nix-ld, envfs, zjstatus, ... }@inputs:
+  outputs =
+    { self, nixpkgs, home-manager, nix-ld, envfs, zjstatus, ... }@inputs:
     let
       lib = nixpkgs.lib;
       system = "x86_64-linux";
       overlays = [
-	(self: super: {
-	  hypr-kblayout = super.callPackage ./pkgs/hypr-kblayout { };
-	  openssh = super.openssh.overrideAttrs ( old: {
-	    patches = (old.patches or [ ]) ++ [ ./patch/openssh.patch ];
-	    doCheck = false;
-	  });
-	  zjstatus = zjstatus.packages.${self.system}.default;
-	})
+        (self: super: {
+          hypr-kblayout = super.callPackage ./pkgs/hypr-kblayout { };
+          openssh = super.openssh.overrideAttrs (old: {
+            patches = (old.patches or [ ]) ++ [ ./patch/openssh.patch ];
+            doCheck = false;
+          });
+          zjstatus = zjstatus.packages.${self.system}.default;
+        })
       ];
       # pkgs = nixpkgs.legacyPackages.${system};
-      pkgs = import nixpkgs {
-	inherit system overlays;
-      };
+      pkgs = import nixpkgs { inherit system overlays; };
     in {
       nixosConfigurations = {
         saturn-vm = lib.nixosSystem {
-	  inherit system;
-	  modules = [ ./host/saturn-vm/configuration.nix ];
-	};
+          inherit system;
+          modules = [ ./host/saturn-vm/configuration.nix ];
+        };
         saturn = lib.nixosSystem {
-	  inherit system;
-	  specialArgs = {
-	    inherit inputs;
-	  };
-	  modules = [ 
-	    ./host/saturn/configuration.nix
+          inherit system;
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./host/saturn/configuration.nix
 
-	    nix-ld.nixosModules.nix-ld
-	    { programs.nix-ld.dev.enable = true; }
+            nix-ld.nixosModules.nix-ld
+            { programs.nix-ld.dev.enable = true; }
 
-	    envfs.nixosModules.envfs
-	  ];
-	};
+            envfs.nixosModules.envfs
+          ];
+        };
         greenbox = lib.nixosSystem {
-	  inherit system;
-	  specialArgs = {
-	    inherit inputs;
-	  };
-	  modules = [ 
-	    ./host/greenbox/configuration.nix 
+          inherit system;
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./host/greenbox/configuration.nix
 
-	    nix-ld.nixosModules.nix-ld
-	    { programs.nix-ld.dev.enable = true; }
+            nix-ld.nixosModules.nix-ld
+            { programs.nix-ld.dev.enable = true; }
 
-	    envfs.nixosModules.envfs
-	  ];
-	};
+            envfs.nixosModules.envfs
+          ];
+        };
       };
       homeConfigurations = {
         "sisyph0s@saturn-vm" = home-manager.lib.homeManagerConfiguration {
-	  inherit pkgs;
-	  modules = [ ./host/saturn-vm/home.nix ];
-	};
+          inherit pkgs;
+          modules = [ ./host/saturn-vm/home.nix ];
+        };
         "sisyph0s@saturn" = home-manager.lib.homeManagerConfiguration {
-	  inherit pkgs;
-	  extraSpecialArgs = {
-              inherit inputs;
-          };
-	  modules = [ 
-	    ./host/saturn/home.nix 
-	    inputs.anyrun.homeManagerModules.anyrun
-	  ];
-	};
+          inherit pkgs;
+          extraSpecialArgs = { inherit inputs; };
+          modules =
+            [ ./host/saturn/home.nix inputs.anyrun.homeManagerModules.anyrun ];
+        };
         "sisyph0s@greenbox" = home-manager.lib.homeManagerConfiguration {
-	  inherit pkgs;
-	  extraSpecialArgs = {
-              inherit inputs;
-          };
-	  modules = [ 
-	    ./host/greenbox/home.nix
-	    inputs.anyrun.homeManagerModules.anyrun
-	  ];
-	};
+          inherit pkgs;
+          extraSpecialArgs = { inherit inputs; };
+          modules = [
+            ./host/greenbox/home.nix
+            inputs.anyrun.homeManagerModules.anyrun
+          ];
+        };
       };
     };
 }
