5f4cc81d26cb591a6713e64d0d9085420c8dbd2f
Pasha Fistanto <pasha@fstn.top>
2025-02-12 21:26:22 +0100

    zellij: disable shell integration

--- common/zellij/default.nix
+++ common/zellij/default.nix
@@ -2,26 +2,7 @@
 {
   programs.zellij = {
     enable = true;
-    # settings = {
-    #   default_shell = "fish";
-    #   copy_command = "wl-copy";
-    #   keybinds = {
-    #     unbind = [ "Ctrl g" ];
-    #     shared_except = {
-    #       _args = [ "locked" ];
-    #       bind = {
-    #         _args = [ "Ctrl m" ];
-    #         SwitchToMode = "locked";
-    #       };
-    #     };
-    #     locked = {
-    #       bind = {
-    #         _args = [ "Ctrl m" ];
-    #         SwitchToMode = "normal";
-    #       };
-    #     };
-    #   };
-    # };
+    enableFishIntegration = false;
   };
   xdg.configFile."zellij".source = ../config/zellij;
   xdg.configFile."zellij/layouts/default.kdl".source = ../config/zellij/layouts/default.kdl;
