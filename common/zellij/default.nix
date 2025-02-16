7ed85fa0e2342d30c50386af17860b4cf6c1095b
Pasha Fistanto <pasha@fstn.top>
2025-02-16 17:24:22 +0100

    zellij: disable zsh integration

--- common/zellij/default.nix
+++ common/zellij/default.nix
@@ -1,8 +1,12 @@
-{ config, pkgs, ... }:
 {
+  config,
+  pkgs,
+  ...
+}: {
   programs.zellij = {
     enable = true;
     enableFishIntegration = false;
+    enableZshIntegration = false;
   };
   xdg.configFile."zellij".source = ../config/zellij;
   xdg.configFile."zellij/layouts/default.kdl".source = ../config/zellij/layouts/default.kdl;
