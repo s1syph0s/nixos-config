49f5cee6783d673dd50ec5a0229f39cfeca6f418
Pasha Fistanto <pasha@fstn.top>
2025-02-27 20:42:13 +0100

    disable ssh support for gpg agent

--- host/common/default.nix
+++ host/common/default.nix
@@ -144,7 +144,7 @@
   # programs.mtr.enable = true;
   programs.gnupg.agent = {
     enable = true;
-    enableSSHSupport = true;
+    # enableSSHSupport = true;
   };
   services.pcscd.enable = true;
 
