dafb57467a688e52b5e03860a454deec59e35a41
Pasha Fistanto <pasha@fstn.top>
2025-03-30 19:25:03 +0200

    add rustfmt

--- home/default.nix
+++ home/default.nix
@@ -144,6 +144,7 @@
 
     # Formatter
     alejandra
+    rustfmt
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
