2d5a366ceb00ae5194d1867aec8d48fa7c266ec8
Pasha Fistanto <pasha@fstn.top>
2025-04-23 12:39:19 +0200

    firewall: whitelist syncthing

--- host/common/default.nix
+++ host/common/default.nix
@@ -183,7 +183,7 @@
   services.openssh.enable = true;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [8384];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
