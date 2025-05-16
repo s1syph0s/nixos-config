0faf9a54396e79037522a77f48f64ce67619721e
Pasha Fistanto <pasha@fstn.top>
2025-05-16 08:49:30 +0200

    johndoe: enable bluetooth

--- host/johndoe/hardware-configuration.nix
+++ host/johndoe/hardware-configuration.nix
@@ -53,4 +53,9 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  # bluetooth
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
 }
