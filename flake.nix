c6bfd4658d77659ececd92bb5733dcc8743e29d5
Pasha Fistanto <pasha@fstn.top>
2025-05-27 23:05:21 +0200

    refactor: overlays

--- flake.nix
+++ flake.nix
@@ -14,34 +14,12 @@
   outputs = {
     self,
     nixpkgs,
-    nixos-hardware,
-    home-manager,
     ...
   } @ inputs: let
     lib = nixpkgs.lib;
     system = "x86_64-linux";
-    overlays = [
-      (self: super: {
-        hypr-kblayout = super.callPackage ./pkgs/hypr-kblayout {};
-        openssh = super.openssh.overrideAttrs (old: {
-          patches = (old.patches or []) ++ [./patch/openssh.patch];
-          doCheck = false;
-        });
-        zjstatus = inputs.zjstatus.packages.${self.system}.default;
-      })
-    ];
-    pkgs = import nixpkgs {
-      inherit system;
-    };
-    pkgs-overlay = {
-      nixpkgs.overlays = overlays;
-    };
   in {
     nixosConfigurations = {
-      saturn-vm = lib.nixosSystem {
-        inherit system;
-        modules = [./host/saturn-vm/configuration.nix];
-      };
       saturn = lib.nixosSystem {
         inherit system;
         specialArgs = {
@@ -49,7 +27,7 @@
         };
         modules = [
           ./host/saturn/configuration.nix
-          pkgs-overlay
+          ./pkgs/overlay.nix
         ];
       };
       greenbox = lib.nixosSystem {
@@ -59,7 +37,7 @@
         };
         modules = [
           ./host/greenbox/configuration.nix
-          pkgs-overlay
+          ./pkgs/overlay.nix
         ];
       };
       johndoe = lib.nixosSystem {
@@ -69,16 +47,8 @@
         };
         modules = [
           ./host/johndoe/configuration.nix
-          pkgs-overlay
-          nixos-hardware.nixosModules.dell-precision-3490
-        ];
-      };
-    };
-    homeConfigurations = {
-      "sisyph0s@saturn-vm" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [
-          ./host/saturn-vm/home.nix
+          ./pkgs/overlay.nix
+          inputs.nixos-hardware.nixosModules.dell-precision-3490
         ];
       };
     };
---
+++ pkgs/overlay.nix
@@ -0,0 +1,20 @@
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}: let
+  overlays = [
+    (self: super: {
+      hypr-kblayout = super.callPackage ./hypr-kblayout {};
+      openssh = super.openssh.overrideAttrs (old: {
+        patches = (old.patches or []) ++ [../patch/openssh.patch];
+        doCheck = false;
+      });
+      zjstatus = inputs.zjstatus.packages.${self.system}.default;
+    })
+  ];
+in {
+  nixpkgs.overlays = overlays;
+}
