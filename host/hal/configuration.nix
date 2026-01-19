4bd37de9524d1f82745b9479c2b9aaa9a1de53e4
Pasha Fistanto <pasha@fstn.top>
2026-01-19 22:07:59 +0100

    hal: fix flaresolver issue

--- host/hal/configuration.nix
+++ host/hal/configuration.nix
@@ -49,6 +49,9 @@
   networking.hostName = "hal"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+  # Disable IPv6 for flaresolverr. Not sure why..
+  networking.enableIPv6 = false;
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
