5d5fbee8118e1efc14e7eb3da654076376664dc9
Pasha Fistanto <fistanto@ibr.cs.tu-bs.de>
2026-03-21 20:10:29 +0100

    bash: remove fish launcher

--- home/default.nix
+++ home/default.nix
@@ -316,11 +316,14 @@
       source ${pkgs.nix-index}/etc/profile.d/command-not-found.sh
 
       # launch fish
-      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-      then
-        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-      fi
+      # FIXME: This is a temporary hack to fix lix problem starting `nix-shell`
+      # https://git.lix.systems/lix-project/lix/issues/1131
+      # 
+      # if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+      # then
+      #   shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+      #   exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+      # fi
     '';
   };
 
