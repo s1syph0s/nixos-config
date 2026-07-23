be7d4832cf559e0e84f39460ef8aa1cc1cdb8697
Pasha Fistanto <pasha@fstn.top>
2026-07-23 10:19:35 +0200

    fish: add zellij auto tab rename function

--- home/default.nix
+++ home/default.nix
@@ -412,6 +412,26 @@
       set -U pure_enable_nixdevshell true
       set -U pure_symbol_nixdevshell_prefix "󱄅 "
       set -U pure_show_subsecond_command_duration true
+
+      function zellij_tab_name_update --on-variable PWD
+          if set -q ZELLIJ
+              set tab_name \'\'
+              if git rev-parse --is-inside-work-tree >/dev/null 2>&1
+                  set git_root (basename (git rev-parse --show-toplevel))
+                  set git_prefix (git rev-parse --show-prefix)
+                  set tab_name "$git_root/$git_prefix"
+                  set tab_name (string trim -c / "$tab_name") # Remove trailing slash
+              else
+                  set tab_name $PWD
+                  if test "$tab_name" = "$HOME"
+                      set tab_name "~"
+                  else
+                      set tab_name (basename "$tab_name")
+                  end
+              end
+              command nohup zellij action rename-tab $tab_name >/dev/null 2>&1 &
+          end
+      end
     '';
     functions = {
       openbg = {
