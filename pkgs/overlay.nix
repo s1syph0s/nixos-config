2467ea2b4211b2b1a2306e5055474c99cfc8b989
Pasha Fistanto <pasha@fstn.top>
2026-09-17 16:27:56 +0200

    update and temp fix

--- pkgs/overlay.nix
+++ pkgs/overlay.nix
@@ -15,6 +15,10 @@
       zjstatus = inputs.zjstatus.packages.${final.system}.default;
       ashell = inputs.ashell.packages.${final.system}.default;
     })
+    # FIXME: Mic92/sops-nix#983
+    (final: prev: {
+      buildGo125Module = prev.buildGoModule;
+    })
   ];
 in {
   nixpkgs.overlays = overlays;
